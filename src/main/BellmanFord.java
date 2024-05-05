5d3b1192937c09c380fc326ec8dbb700209bea56
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2024-05-05 22:51:09 +0300

    Bellman-Ford-Algorithm (#5)
    * Add empty readme
    
    * Adding templates to fill for code
    
    * Finish basic code for Floyd (untested)
    - note: if floydAllPaths[u][v] is empty (NOT NULL): no path
    
    * Make print path
    
    * Floyd ready to be tested
    
    * Integrate Floyd with CLI
    
    * Implement Strategy DP and integrate with CLI & Floyd
    
    * Adding Dijkstra algorithm
    
    * Adding Dijkstra algorithm
    
    * Small Updates
    
    * separate Bellman (need testing )
    
    ---------
    
    Co-authored-by: احمد يوسف صبحى محمد الجعرانى <108885178+ahmedyoussefg@users.noreply.github.com>
    Co-authored-by: AbdElRahman Osama <bodyusama207@gmail.com>

--- src/main/BellmanFord.java
+++ src/main/BellmanFord.java
@@ -1,37 +1,132 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class BellmanFord implements Algorithm {
+   
+    private Map<Integer, List<Edge>> graph;
+    private int numberOfNodes;
+    private int[][] costMatrix;
+    private int[][] parent;
+    private boolean containsNegativeCycle;
+
+
+    private void init() {
+        this.costMatrix = new int[numberOfNodes][numberOfNodes];
+        for (int i = 0; i < numberOfNodes; i++) {
+            for (int j = 0; j < numberOfNodes; j++) {
+                if (i == j)
+                    this.costMatrix[i][j] = 0;
+                else
+                    this.costMatrix[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        this.parent = new int[numberOfNodes][numberOfNodes];
+        for (int i = 0; i < numberOfNodes; i++) {
+            for (int j = 0; j < numberOfNodes; j++) {
+                this.parent[i][j] = -1;
+            }
+        }
+    }
 
     @Override
     public boolean calculateShortestPathsFromSource(int source) {
-        return false;
+        for (int i = 0; i < numberOfNodes; i++) {
+            costMatrix[source][i] = Integer.MAX_VALUE;
+            parent[source][i] = -1;
+        }
+        costMatrix[source][source] = 0;
+
+        for (int i = 0; i < numberOfNodes - 1; i++) {
+            for (int src = 0; src < numberOfNodes; src++) {
+                for (Edge edge : graph.get(src)) {
+                    int dest = edge.getDestination();
+                    int weight = edge.getWeight();
+                    if (costMatrix[source][src] != Integer.MAX_VALUE && costMatrix[source][src] + weight < costMatrix[source][dest]) {
+                        costMatrix[source][dest] = costMatrix[source][src] + weight;
+                        parent[source][dest] = src;
+                    }
+                }
+            }
+        }
+        for (int src = 0; src < numberOfNodes; src++) {
+            for (Edge edge : graph.get(src)) {
+                int dest = edge.getDestination();
+                int weight = edge.getWeight();
+                if (costMatrix[source][src] != Integer.MAX_VALUE && costMatrix[source][src] + weight < costMatrix[source][dest]) {
+                    containsNegativeCycle = true;
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean calculateAllPairsShortestPaths() {
-        return false;
+        for (int i = 0; i < numberOfNodes; i++) {
+            calculateShortestPathsFromSource(i);
+        }
+        // return false;
+        return !containsNegativeCycle;
     }
-
+    
     @Override
     public List<Integer> getPath(int u, int v) {
-        return null;
+
+        if (parent[u][v] == -1) {
+            return null;
+        }
+        List<Integer> path = new ArrayList<>();
+        int temp = v;
+        while (temp != u) {
+            path.add(temp);
+            temp = parent[u][temp];
+        }
+        path.add(u);
+        Collections.reverse(path);
+        return path;
+
     }
 
     @Override
     public int getCost(int u, int v) {
-        return 0;
+        return costMatrix[u][v];
     }
 
     @Override
     public void setNumberOfNodes(int numberOfNodes) {
-
+        this.numberOfNodes = numberOfNodes;
+        init();
     }
 
     @Override
     public void setGraph(Map<Integer, List<Edge>> graph) {
-
+        this.graph = graph;
+    }
+    
+    public boolean containsNegativeCycle(){
+        // if (containsNegativeCycle == true)
+        //     return true;
+        boolean visited[] = new boolean[numberOfNodes];
+        Arrays.fill(visited, false);
+       
+        
+        for (int src = 0; src < numberOfNodes; src++){
+            if (!visited[src]){
+                if (!calculateShortestPathsFromSource(src)){
+                    return true;
+                }
+                for (int j = 0; j < numberOfNodes; j++)
+                        if ( costMatrix[src][j] != Integer.MAX_VALUE)
+                            visited[j] = true;
+            }
+        
+        }
+        return false;
     }
 }
