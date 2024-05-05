62cafdc9b5c952408b2930c225cf218c11b46e9f
AbdElRahman Osama <bodyusama207@gmail.com>
2024-05-05 20:37:11 +0300

    Adding Dijkstra algorithm

---
+++ src/main/Edge.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Edge {
+    private int weight;
+    private int destination;
+
+    public Edge(int destination, int weight) {
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public int getDestination() {
+        return destination;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
