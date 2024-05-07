8e40f3236fbeb73aa16b404f4414b9744e8c28fe
Bassam <abdelrahmanbassam0109@gmail.com>
2024-05-07 15:59:53 +0300

    add tests' images

--- src/test/BellmanFordTest.java
+++ src/test/BellmanFordTest.java
@@ -15,19 +15,37 @@ public class BellmanFordTest {
    
    @Test
    public void negativeCycleSmallTest() {
-       Graph graph = new Graph("graph_files\\negativeCycleSmall.txt");
+       Graph graph = new Graph("graph_files\\negativeCycleSmall\\negativeCycleSmall.txt");
        Algorithm algorithm = new BellmanFord();
        graph.setAlgorithm(algorithm);
        boolean isNegativeCycle=!graph.calculateShortestPaths();
        assertEquals(true, isNegativeCycle);   
    }
+   
    @Test
-   public void negativeCycleSmallTest() {
-       Graph graph = new Graph("graph_files\\negativeCycleSmall.txt");
+   public void negativeCycleSmallFromSourceTest() {
+       Graph graph = new Graph("graph_files\\negativeCycleSmall\\negativeCycleSmall.txt");
+       Algorithm algorithm = new BellmanFord();
+       graph.setAlgorithm(algorithm);
+       boolean isNegativeCycle=!graph.calculateShortestPaths(0);
+       assertEquals(true, isNegativeCycle);   
+   }
+   @Test
+   public void noNegativeCycleSmallTest() {
+       Graph graph = new Graph("graph_files\\noNegativeCycleSmall\\noNegativeCycleSmall.txt");
        Algorithm algorithm = new BellmanFord();
        graph.setAlgorithm(algorithm);
        boolean isNegativeCycle=!graph.calculateShortestPaths();
-       assertEquals(true, isNegativeCycle);   
+       assertEquals(false, isNegativeCycle);   
+   }
+   
+   @Test
+   public void noNegativeCycleSmallFromSourceTest() {
+       Graph graph = new Graph("graph_files\\noNegativeCycleSmall\\\\noNegativeCycleSmall.txt");
+       Algorithm algorithm = new BellmanFord();
+       graph.setAlgorithm(algorithm);
+       boolean isNegativeCycle=!graph.calculateShortestPaths(0);
+       assertEquals(false, isNegativeCycle);   
    }
 
 }
